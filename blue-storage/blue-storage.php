f79caf0ac8314b66c047cf60cb0661e8fb3d2380
Derek Held <derekheld@users.noreply.github.com>
2019-05-26 10:53:42 -0700

    Include our new class file

--- blue-storage/blue-storage.php
+++ blue-storage/blue-storage.php
@@ -12,6 +12,7 @@
 require_once( 'class-blue-storage-const.php' );
 require_once( 'class-blue-storage-settings.php' );
 require_once( 'class-azure-storage-client.php' );
+require_once( 'class-blue-storage-utility.php' );
 require_once ( 'libraries/httpful.phar' ); // Include here because we make a template to be used across the whole plugin
 
 \BlueStorage\BlueStorageSettings::init();
